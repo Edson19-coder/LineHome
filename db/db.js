b4b12515f3f917226349989c21bc064690fbea45
edson.lugo <edson.lugo@netpay.com.mx>
2021-05-08 19:52:49 -0500

    change prod db

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Solido20~',
     database: 'dblinehome'
 });
 
